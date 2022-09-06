e15644eb80244f56175e2505bfc37aec2314f90c
Emad Kheiralla <emadkheiralla@hotmail.com>
2022-09-06 18:22:27 -0400

    added tests

--- randstad-test/src/app/questionaire/questionaire.component.spec.ts
+++ randstad-test/src/app/questionaire/questionaire.component.spec.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { QuestionaireComponent } from './questionaire.component';
 
 describe('QuestionaireComponent', () => {
@@ -20,4 +20,20 @@ describe('QuestionaireComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change radio btn with fakeAsync', fakeAsync(() => {
+    let buttonElement = fixture.debugElement.query(By.css('.my-radio-btn'));
+      
+    spyOn(component, 'changeAnswer');
+    spyOn(component.answer, 'emit');
+
+    //Trigger click event after spyOn
+    buttonElement.triggerEventHandler('change', 'Red');
+    tick();
+    expect(component.changeAnswer).toHaveBeenCalledWith('Red');
+    expect(component.form.valid).toBeTruthy();
+    fixture.detectChanges();
+    // expect(component.answer.emit).toHaveBeenCalled();
+  }));
+
 });
